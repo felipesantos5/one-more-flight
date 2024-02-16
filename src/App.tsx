6e069c49a253de8dbedc1d474a3c67f4571f6da9
Aurelio <aureliofrainer@gmail.com>
2024-02-16 14:00:17 -0300

    refactor: Remove header from Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "./components/footer/footer";
-import { Header } from "./components/header/header";
 
 function App() {
   return (
     <div className="">
-      <Header />
       <Outlet />
       <Footer />
     </div>
